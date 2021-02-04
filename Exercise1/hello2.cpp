c35ed660fb9264583dabbf9917b359cbe7313bd3
Edward Webster <ew639@bath.ac.uk>
2021-02-04 09:34:42 +0000

    Completed Exercise 1 Added glSimple.cpp Completed first Lab Awaiting LOIL

--- Exercise1/hello2.cpp
+++ Exercise1/hello2.cpp
@@ -2,10 +2,16 @@
 
 using namespace std;
 
-int main( int argc, char *argv[] ) {
+int main(int argc, char *argv[]) {
+
     cout << "Hello world!" << endl;
     cout << "Hit enter to exit..." << endl;
+
+    int x = 4;
+    printf("x is %d", x);
+
     char readChar;
     readChar = cin.get();
+
     return 0;
 }
--- Exercise1/planetClass.cpp
+++ Exercise1/planetClass.cpp
@@ -1,42 +1,44 @@
 #include <iostream>
-#include <stdio.h>
 // following line is needed so that M_PI gives value of pi
 #define _USE_MATH_DEFINES
+
 #include <math.h>
 
 using namespace std;
 
 class Planet {
-  public:
+public:
     double mass;
     double radius;
-    double Volume() { 
-      return 4.0 * M_PI * radius * radius * radius / 3.0;
-    } 
+
+    double Volume() {
+        return 4.0 * M_PI * radius * radius * radius / 3.0;
+    }
+
     double Density() {
-      return mass / Volume();
+        return mass / Volume();
     }
 };
 
-int main( int argc, char *argv[] ) {
-  // declare a new planet
-  Planet earth;
+int main() {
+    // declare a new planet
+    Planet earth;
 
-  earth.mass = 6e24;
-  earth.radius = 6.4e6;
+    earth.mass = 6e24;
+    earth.radius = 6.4e6;
 
-  double density = earth.Density();
+    double density = earth.Density();
 
-  cout << "earth density is " << density << " (SI units, accuracy 1 sig fig)" << endl;
+    cout << "earth density is " << density << " (SI units, accuracy 1 sig fig)" << endl;
 
-  Planet jupiter;
+    Planet jupiter;
 
-  jupiter.mass = 1.9e27;
-  jupiter.radius = 7e7;
-  
-  cout << "jupiter density is " << jupiter.Density() << " (SI units, accuracy 1 sig fig)" << endl;
+    jupiter.mass = 1.9e27;
+    jupiter.radius = 7e7;
 
-  cout << "Hit enter to exit" << endl;
-  cin.get();
-  return 0;
+    cout << "jupiter density is " << jupiter.Density() << " (SI units, accuracy 1 sig fig)" << endl;
+
+    cout << "Hit enter to exit" << endl;
+    cin.get();
+    return 0;
 }
